b1a7056e3fef167d0c79a442e51ce62fd82c03f0
Apple <apple@Apples-MacBook-Pro.local>
2018-04-13 17:53:00 +0700

    set up activationFunction

---
+++ activationFunction.py
@@ -0,0 +1,16 @@
+import numpy as np 
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+def dSigmoid(x) : //đạo hàm của sigmoid 
+   return sigmoid(x) * (1 - sigmoid(x))
+
+def ReLU(x) : 
+    return x * (x > 0)
+
+def softmax(x):       // activation when output là multiclass 
+    e = numpy.exp(x - numpy.max(x))  # prevent overflow
+    if e.ndim == 1:
+        return e / numpy.sum(e, axis=0)
+    else:  
+        return e / numpy.array([numpy.sum(e, axis=1)]).T  # ndim = 2
--- mnist.py
+++ mnist.py
@@ -2,3 +2,4 @@ import mnist_reader
 X_train, y_train = mnist_reader.load_mnist('data/fashion', kind='train')
 X_test, y_test = mnist_reader.load_mnist('data/fashion', kind='t10k')
 print (X_train.shape[0], X_train.shape[1])
+print (y_train.shape[0], y_train.shape[1])
