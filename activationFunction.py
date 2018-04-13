9ed54d19edc5f9e00ca1dd896479a84a3ec14e58
Apple <apple@Apples-MacBook-Pro.local>
2018-04-13 17:56:37 +0700

    a

--- activationFunction.py
+++ activationFunction.py
@@ -2,14 +2,14 @@ import numpy as np
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
-def dSigmoid(x) : //đạo hàm của sigmoid 
+def dSigmoid(x) : #đạo hàm của sigmoid 
    return sigmoid(x) * (1 - sigmoid(x))
 
 def ReLU(x) : 
     return x * (x > 0)
 
-def softmax(x):       // activation when output là multiclass 
-    e = numpy.exp(x - numpy.max(x))  # prevent overflow
+def softmax(x):       # activation when output là multiclass 
+    e = numpy.exp(x - numpy.max(x))  # ngăn không bị tràn số 
     if e.ndim == 1:
         return e / numpy.sum(e, axis=0)
     else:  
