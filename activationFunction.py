b6cae2e833a07b9f87da0f1259f573b7b6899e37
Apple <apple@Apples-MacBook-Pro.local>
2018-04-14 14:16:00 +0700

    function setup

--- activationFunction.py
+++ activationFunction.py
@@ -1,16 +1,35 @@
 import numpy as np 
-def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
+def sigmoid(Z):
+    A = 1/(1+np.exp(-Z))
+    cache = Z
+    return A, cache
 
-def dSigmoid(x) : #đạo hàm của sigmoid 
-   return sigmoid(x) * (1 - sigmoid(x))
+def dSigmoid(dA, cache):
+    Z = cache
+    s = 1/(1+np.exp(-Z))
+    dZ = dA * s * (1-s)
+    assert (dZ.shape == Z.shape)
+    return dZ
 
-def ReLU(x) : 
-    return x * (x > 0)
-
-def softmax(x):       # activation when output là multiclass 
-    e = numpy.exp(x - numpy.max(x))  # ngăn không bị tràn số 
+def relu(Z):
+    A = np.maximum(0,Z)
+    assert(A.shape == Z.shape)
+    cache = Z
+    return A, cache
+def dReLU(dA, cache): # đạo hàm của hàm ReLU
+    Z = cache
+    dZ = np.array(dA, copy=True) # just converting dz to a correct object.
+    # When z <= 0, you should set dz to 0 as well. 
+    dZ[Z <= 0] = 0
+    assert (dZ.shape == Z.shape)
+    return dZ
+def softmax(Z):       # activation when output là multiclass 
+    e = numpy.exp(Z - numpy.max(Z))  # ngăn không bị tràn số 
+    A = Z
     if e.ndim == 1:
-        return e / numpy.sum(e, axis=0)
+        A =  e / numpy.sum(e, axis=0)
     else:  
-        return e / numpy.array([numpy.sum(e, axis=1)]).T  # ndim = 2
+        A = e / numpy.array([numpy.sum(e, axis=1)]).T  # ndim = 2
+    cache = Z 
+    return A, cache
+def dSoftmax(dA, cache):
--- mnist.py
+++ mnist.py
@@ -1,5 +1,60 @@
 import mnist_reader
+from activationFunction import sigmoid, dSigmoid, ReLU, dReLU, softmax
+import numpy as np
 X_train, y_train = mnist_reader.load_mnist('data/fashion', kind='train')
 X_test, y_test = mnist_reader.load_mnist('data/fashion', kind='t10k')
-print (X_train.shape[0], X_train.shape[1])
-print (y_train.shape[0], y_train.shape[1])
+m = X_train.shape[0]
+def initialize_parameters(layer_dims): 
+    parameters = {}
+    L = len(layer_dims)
+    for i in range(1, L):
+        parameters['W' + str(i)] = np.random.randn(layer_dims[i], layer_dims[i-1]) * 0.01
+        parameters['b' + str(i)] = np.zeros(shape = (layer_dims[i],1))
+
+    return parameters
+
+def linear_forward_propagation(A, W, b):
+    Z = np.dot(W, A) + b
+    cache = (A, W, b)
+    return Z, cache
+
+
+def forward_propagation(A_prev, W,b, activation):
+    if activation == "sigmoid":
+        Z, linear_cache = linear_forward_propagation(A_prev, W, b)
+        A, activation_cache = sigmoid(Z)
+    if activation == "ReLU":
+         Z, linear_cache = linear_forward_propagation(A_prev, W, b)
+         A, activation_cache = ReLU(Z)
+    if activation == "softmax":
+        Z, linear_cache = linear_forward_propagation(A_prev, W, b)
+        A, activation_cache = softmax(Z)
+    cache = (linear_cache, activation_cache)
+    return A, cache
+
+def cost_function(AL, y) :
+   cost = -1 / m *np.sum(np.multiply(np.log(AL), Y) + np.multiply(1-Y, np.log(1-AL)))
+   return cost 
+
+def linear_backward(dZ, cache):
+    A_prev, W, b = cache
+    m = A_prev.shape[1]
+    dW = np.dot(dZ, A_prev.T) / m
+    db = np.sum(dZ, axis=1, keepdims=True) / m
+    dA_prev = np.dot(W.T, dZ)
+    assert (dA_prev.shape == A_prev.shape)
+    assert (dW.shape == W.shape)
+    assert (db.shape == b.shape)
+    return dA_prev, dW, db
+
+
+
+    
+
+# def main() :
+#     parameters = initialize_parameters([5,4,1])
+#     print(parameters)
+
+# if (__name__ == "__main__"):
+#     main()
+   
